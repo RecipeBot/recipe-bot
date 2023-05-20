5046cb2ba9550dfcdac195dd08ba8abe7a996e19
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-05-20 14:36:20 -0300

    :sparkles: feat: Formulário de ingredientes

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,13 +1,17 @@
+import { type } from "os";
+
 interface ButtonProps {
   children: string;
-  onClick: () => void;
+  onClick?: () => void;
+  type?: 'button' | 'submit' | 'reset';
 }
 
 export const Button = (props: ButtonProps) => {
-  const { children, onClick } = props
+  const { children, onClick, type } = props
 
   return (
     <button
+      type={type}
       className="bg-brand-500 hover:brightness-110 transition-all text-white font-bold py-4 px-8 rounded"
       onClick={onClick}
     >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,52 @@ import { Inter } from 'next/font/google'
 import { Input } from '@/components/base/Input'
 import { Button } from '@/components/base/Button'
 import { Robot } from 'phosphor-react'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const RecipeFormSchema = z.object({
+  ingredients: z.string().nonempty('Insira pelo menos um ingrediente')
+})
+
+type RecipeFormSchemaType = z.infer<typeof RecipeFormSchema>
+
 export default function Home() {
+
+  const { control, handleSubmit } = useForm<RecipeFormSchemaType>({
+    defaultValues: {
+      ingredients: ''
+    },
+    resolver: zodResolver(RecipeFormSchema)
+  })
+
+  const onSubmit = (data: RecipeFormSchemaType) => {
+    console.log(data)
+  }
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-center gap-8 p-16 bg-bg-df ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center gap-6 p-10 bg-bg-df ${inter.className}`}
     >
       <div className='flex items-center justify-start gap-2'>
         <Robot size={48} weight="bold" />
-        <h1 className='text-2xl font-bold text-zinc-900 lg:text-4xl'>Recipe AI</h1>
+        <h1 className='text-2xl font-bold text-zinc-900 lg:text-4xl'>Nome do app</h1>
       </div>
-      <div className="flex z-10 w-full flex-col gap-8 max-w-4xl items-center justify-between text-sm">
-        <Input type="text" onChange={console.log} placeholder='Insira seus ingredientes aqui' />
-        <Button onClick={() => { }}>Criar Receita</Button>
-      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex z-10 w-full flex-col gap-6 max-w-4xl items-center justify-between text-sm"
+      >
+        <Controller
+          name='ingredients'
+          control={control}
+          render={({ field: { onChange } }) => (
+            <Input type="text" onChange={(value) => onChange(value)} placeholder='Insira seus ingredientes aqui' />
+          )}
+        />
+        <Button type="submit">Criar Receita</Button>
+      </form>
     </main>
   )
 }
