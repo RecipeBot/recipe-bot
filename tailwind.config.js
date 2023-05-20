b3cefa3cdb766869261bd0d5fc0fbe5b35d80e9f
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-05-20 14:05:45 -0300

    :lipstick: style: Global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        brand: {
+          300: '#fdba74',
+          400: '#fb923c',
+          500: '#f97316',
+        },
+        bg: {
+          df: '#fafafa',
+        }
+      },
     },
   },
   plugins: [],
