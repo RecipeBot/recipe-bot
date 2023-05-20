a9e9ba7a2f5daa0c93f5fbfa0f16145a067ee7f5
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-05-20 18:32:11 -0300

    :bug: fix: Adding missing await keyword

--- src/pages/api/recipe/index.ts
+++ src/pages/api/recipe/index.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
-    case "GET": getRecipe(req, res); break;
+    case "GET": await getRecipe(req, res); break;
     default: {
       res.status(405).json({ message: "Method not allowed" })
     }
